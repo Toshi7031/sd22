2b9ec08a8b366cf900ce5f58ba11a5364d38670b
kosa804 <fazsegtwin@gmail.com>
2019-08-06 13:24:32 +0900

    ajaxテストに成功

---
+++ controller/buy.php
@@ -0,0 +1,13 @@
+<?PHP
+//*************************
+//課題No.00
+//作成日:2019//
+//作成者:峯松康二
+//クラス:IH-12A-905
+//*************************
+
+require_once '../config/config.php';
+require_once '../func/func.php';
+
+
+require_once '../tpl/buy.php';
---
+++ controller/login.php
@@ -0,0 +1,14 @@
+<?PHP
+//*************************
+//課題No.00
+//作成日:2019//
+//作成者:峯松康二
+//クラス:IH-12A-905
+//*************************
+
+session_start();
+require_once '../config/config.php';
+require_once _FUNC_FILE;
+
+
+require_once '../view/login.php';
