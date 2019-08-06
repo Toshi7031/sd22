8fe56402a5b04c4d2178711d344f6839cf3f4477
kosa804 <fazsegtwin@gmail.com>
2019-08-06 23:46:01 +0900

    不要行削除

--- js/login.js
+++ js/login.js
@@ -1,25 +1,23 @@
 $(function() {
   $("#login").on("click", function() {
-    // console.log('click');
     let name = $('#login_form [name="name"]').val();
     let pass = $('#login_form [name="password"]').val();
     //デバッグ用
-    console.log(name);
-    console.log(pass);
+    // console.log(name);
+    // console.log(pass);
 
     $.ajax({
-      url: '../model/ajax/read_user_data.php',
+      url: "../model/ajax/read_user_data.php",
       type: "POST",
       catch: false,
-      dataType: 'json',
-      data: {name, pass}
+      dataType: "json",
+      data: { name, pass }
     })
       .done(function(data) {
+        //デバッグ用
         console.log(data.name);
         console.log(data.pass);
-        // console.log(data[0]);
-        // $('#error_mes_name').html(data.name);
-        // $('#error_mes_pass').html(data.pass);
+        //正式な処理を追加する
       })
       .fail(function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR);
