169c88f8c21e514c369c89514389b77416955ef3
kozo804 <fazsegtwin@gmail.com>
2019-12-10 16:55:14 +0900

    ファイル作成

--- controller/s_sell.php
+++ controller/s_sell.php
@@ -1,14 +1,9 @@
 <?PHP
-//*************************
-//課題No.00
-//作成日:2019//
-//作成者:峯松康二
-//クラス:IH-12A-905
-//*************************
 
 session_start();
 require_once '../config/config.php';
-require_once _FUNC_FILE;
+require_once '../model/func/func.php';
+
 
 
 require_once '../view/login.php';
