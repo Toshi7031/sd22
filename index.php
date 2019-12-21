65be071c5786f1d845bf0cda0a919cdfd4c0aa24
kozo804 <fazsegtwin@gmail.com>
2019-12-21 15:57:07 +0900

    session削除

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?PHP
 
-session_start();
+//configと関数呼び出し
 require_once './config/config.php';
 require_once './model/func/func.php';
 
 
+//データベースから新着商品を15件取ってくる処理
 
+
+//表示部の読み出し
 require_once './view/index.php';
