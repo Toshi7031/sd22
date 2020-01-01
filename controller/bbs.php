d522ebf3515d74932f52615ff68cd64b2e44a4c0
Toshi7031 <7031.toshi@gmail.com>
2020-01-01 22:09:00 +0900

    最初

--- controller/bbs.php
+++ controller/bbs.php
@@ -1,3 +1,15 @@
 <?php
-  require_once '../view/bbs.html';
+//require
+require_once'../config/config.php';
+require_once'../model/func/func.php';
+//データベース接続
+//ajaxからのpostを受け取る(if)
+//書き込み(endif)
+//トーク内容取り出し
+//ログインセッションから自分のIDを取り出し
+//自分のトーク内容と相手のトーク内容を分ける
+//トーク内容print
+//ajaxを利用して随時読み込み
+
+  require_once '../view/bbs.php';
 ?>
---
+++ view/bbs.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>掲示板</title>
+    <link rel="stylesheet" href="../css/bbs.css">
+</head>
+<body>
+<div class="wrapp">
+   <div class="bbs_top">
+       <ul>
+           <li><button class="history_back" type="button" onclick="history.back()"><img src="../images/image_materials/arrow.png" alt=""></button><span>掲示板</span></li>
+           <li><h2 class="bbs_name"><?php //echo $thred_name ?></h2></li>
+           <li><img class="title" src="../images/image_materials/logo.png" alt="まさる堂"></li>
+       </ul>
+   </div>
+   <div class="bbs_contents">
+        <ul>
+            <?php //echo トーク内容 ?>
+            <li class="you"><img class="talk_image" src="../images/icons/yjimage.jpeg" alt=""><span>名前</span><p class="talk">トーク内容1</p></li>
+            <li class="you"><img class="talk_image" src="../images/icons/yjimage.jpeg" alt=""><span>名前</span><p class="talk">トーク内容2</p></li>
+            <li class="you"><img class="talk_image" src="../images/icons/yjimage.jpeg" alt=""><span>名前</span><p class="talk">トーク内容3</p></li>
+            <li class="you"><img class="talk_image" src="../images/icons/yjimage.jpeg" alt=""><span>名前</span><p class="talk">トーク内容4</p></li>
+            <li class="me"> <p class="talk">トーク内容5</p> </li>
+        </ul>
+        <div class="talk_send">
+            <form action="" method="post" class='flex'>
+                    <textarea class="send_text" name="text" cols="30"></textarea>
+                    <button type="submit"><img src="../images/image_materials/plane.png" alt=""></button>
+            </form>
+        </div>
+   </div>
+</div>
+</body>
+</html>
