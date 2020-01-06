5566d43b4823ff7e84fbf739a97c06ba7e8a5c19
kozo804 <fazsegtwin@gmail.com>
2020-01-06 22:42:37 +0900

    css追加

--- controller/reset_password_complete.php
+++ controller/reset_password_complete.php
@@ -5,9 +5,11 @@ require_once '../config/config.php';
 require_once '../model/func/func.php';
 
 //はじく処理
-if($_SESSION['changed_password'] !== true) {
-  redirect('../index.php');
-}
-$_SESSION['changed_password'] = false;
+// if($_SESSION['changed_password'] !== true) {
+//   redirect('../index.php');
+// }
+// $_SESSION['changed_password'] = false;
+
+$css_file_name = 'reset_password_complete.css';
 
 require_once '../view/reset_password_complete.php';
