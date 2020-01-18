69d6ba92bc1e8383cf7363a3538519d9288ca6a4
kozo804 <fazsegtwin@gmail.com>
2020-01-18 22:08:05 +0900

    非ログイン時に会員登録へのリンクを設置

--- view/header.php
+++ view/header.php
@@ -9,6 +9,7 @@
   <title><?php echo $title; ?></title>
   <link href="../css/<?php echo $css_file_name; ?>" rel="stylesheet" type="text/css">
   <script src="../js/jquery-3.4.1.min.js"></script>
+  <script src="../js/header.js"></script>
 </head>
 
 <body>
@@ -20,19 +21,100 @@
         <button>検索</button>
       </div>
       <div class="header_login_area">
-<?php if (isset($_SESSION['login_id']) && !empty($_SESSION['login_id'])) : ?>
-        <p><a href="./controller/logout.php">ログアウト</a></p>
-        <p><a href="./controller/mypage_notification.php">マイページ</a></p>
-<?php else:?>
-        <p><a href="./controller/login.php">ログイン</a></p>
-<?php endif; ?>
+        <?php if (isset($_SESSION['login_id']) && !empty($_SESSION['login_id'])) : ?>
+          <p><a href="./controller/logout.php">ログアウト</a></p>
+          <p><a href="./controller/mypage_notification.php">マイページ</a></p>
+        <?php else : ?>
+          <p><a href="./controller/login.php">ログイン</a></p>
+          <p><a href="./controller/sign_in.php">新規会員登録</a></p>
+        <?php endif; ?>
       </div>
     </header><!-- global_navi -->
     <nav>
       <ul>
-        <li><a href="">商品ジャンル一覧</a></li>
-        <li><a href="">掲示板一覧</a></li>
-        <li><a href="./s_sell.php">出品</a></li>
+        <li id="select_product_category">
+          <p>商品ジャンル一覧</p>
+          <div id="category_list">
+            <p>商品一覧</p>
+            <p class="cancel_button">×</p>
+            <ul id="large_category_list">
+              <li>レディース</li>
+              <li>メンズ</li>
+              <li>キッズ</li>
+              <li>インテリア・住まい</li>
+              <li>家電</li>
+              <li>ホビー・娯楽</li>
+              <li>ハンドメイド</li>
+            </ul>
+            <div id="small_category_area">
+              <ul class="small_category_list">
+                <li>服</li>
+                <li>帽子</li>
+                <li>アクセサリー</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>服</li>
+                <li>帽子</li>
+                <li>アクセサリー</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>ベビー服</li>
+                <li>キッズ服</li>
+                <li>キッズ靴</li>
+                <li>子供用ファッション小物</li>
+                <li>外出・移動用具</li>
+                <li>ベビー家具・寝具・室内用品</li>
+                <li>おもちゃ</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>キッチン・食器</li>
+                <li>ベッド・マットレス・寝具</li>
+                <li>机・いす</li>
+                <li>収納寝具</li>
+                <li>カーペット・ラグ・マット</li>
+                <li>照明</li>
+                <li>時計</li>
+                <li>インテリア小物</li>
+                <li>季節・年中行事</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>スマホ</li>
+                <li>スマホアクセサリー</li>
+                <li>PC・タブレット</li>
+                <li>カメラ</li>
+                <li>テレビ・映像機器</li>
+                <li>オーディオ機器</li>
+                <li>美容・健康</li>
+                <li>冷暖房・空調</li>
+                <li>生活家電</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>本・DVD・音楽</li>
+                <li>ゲーム</li>
+                <li>スポーツ・レジャー</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>アクセサリー(女性)</li>
+                <li>ファッション・小物</li>
+                <li>アクセサリー・時計</li>
+                <li>日用品・インテリア</li>
+                <li>趣味・おもちゃ</li>
+                <li>素材・材料</li>
+                <li>その他</li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li id="select_bbs_category">
+          <p>掲示板一覧</p>
+        </li>
+        <li><a href="./controller/exhibition_sell.php">出品</a></li>
         <li><a href="">お問い合わせ</a></li>
       </ul>
     </nav><!-- top_navi -->
--- view/index.php
+++ view/index.php
@@ -9,6 +9,7 @@
   <title>まさる堂/トップページ</title>
   <link href="./css/index.css" rel="stylesheet" type="text/css">
   <script src="./js/jquery-3.4.1.min.js"></script>
+  <script src="./js/header.js"></script>
 </head>
 
 <body>
@@ -25,40 +26,119 @@
           <p><a href="./controller/mypage_notification.php">マイページ</a></p>
         <?php else : ?>
           <p><a href="./controller/login.php">ログイン</a></p>
+          <p><a href="./controller/sign_in.php">新規会員登録</a></p>
         <?php endif; ?>
       </div>
     </header><!-- global_navi -->
     <nav>
       <ul>
-        <li><a href="">商品ジャンル一覧</a></li>
+      <li id="select_product_category">
+          <p>商品ジャンル一覧</p>
+          <div id="category_list">
+            <p>商品一覧</p>
+            <p class="cancel_button">×</p>
+            <ul id="large_category_list">
+              <li>レディース</li>
+              <li>メンズ</li>
+              <li>キッズ</li>
+              <li>インテリア・住まい</li>
+              <li>家電</li>
+              <li>ホビー・娯楽</li>
+              <li>ハンドメイド</li>
+            </ul>
+            <div id="small_category_area">
+              <ul class="small_category_list">
+                <li>服</li>
+                <li>帽子</li>
+                <li>アクセサリー</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>服</li>
+                <li>帽子</li>
+                <li>アクセサリー</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>ベビー服</li>
+                <li>キッズ服</li>
+                <li>キッズ靴</li>
+                <li>子供用ファッション小物</li>
+                <li>外出・移動用具</li>
+                <li>ベビー家具・寝具・室内用品</li>
+                <li>おもちゃ</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>キッチン・食器</li>
+                <li>ベッド・マットレス・寝具</li>
+                <li>机・いす</li>
+                <li>収納寝具</li>
+                <li>カーペット・ラグ・マット</li>
+                <li>照明</li>
+                <li>時計</li>
+                <li>インテリア小物</li>
+                <li>季節・年中行事</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>スマホ</li>
+                <li>スマホアクセサリー</li>
+                <li>PC・タブレット</li>
+                <li>カメラ</li>
+                <li>テレビ・映像機器</li>
+                <li>オーディオ機器</li>
+                <li>美容・健康</li>
+                <li>冷暖房・空調</li>
+                <li>生活家電</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>本・DVD・音楽</li>
+                <li>ゲーム</li>
+                <li>スポーツ・レジャー</li>
+                <li>その他</li>
+              </ul>
+              <ul class="small_category_list">
+                <li>アクセサリー(女性)</li>
+                <li>ファッション・小物</li>
+                <li>アクセサリー・時計</li>
+                <li>日用品・インテリア</li>
+                <li>趣味・おもちゃ</li>
+                <li>素材・材料</li>
+                <li>その他</li>
+              </ul>
+            </div>
+          </div>
+        </li>
         <li><a href="">掲示板一覧</a></li>
-        <li><a href="./s_sell.php">出品</a></li>
+        <li><a href="./controller/exhibition_sell.php">出品</a></li>
         <li><a href="">お問い合わせ</a></li>
       </ul>
     </nav><!-- top_navi -->
     <div id="branding">
-      <h2><img src="./images/image_materials/branding_01.jpg" width="700" height="350" alt=""></h2>
+      <!-- <h2></h2> -->
     </div><!-- branding -->
     <div id="contents_area">
       <h2>新着商品</h2>
-<?php if(count($products) !== 0):?>
-      <ul>
-<?php foreach($products as $value):?>
-          <li class="products">
-            <a href="./controller/product_deteals.php?id=<?php echo $value['id'] ?>">
-              <ul>
-                <li class="product_img"><img src="./images/products/product_<?php echo $value['id'] ?>_1.jpg" width="180px" height="180px" alt=""></li>
-                <li class="product_name"><?php echo $value['product_name']; ?></li>
-                <li class="product_price">￥<?php echo $value['price']; ?></li>
-                <li class="favorite"><img src="./images/image_materials/hart.jpg" width="30" height="30" alt=""></li>
-              </ul>
-            </a>
-          </li>
-<?php endforeach;?>
-      </ul>
-<?php else:?>
-      <p><?php echo $error_msg;?></p>
-<?php endif;?>
+      <?php if (count($products) !== 0) : ?>
+        <ul>
+          <?php foreach ($products as $value) : ?>
+            <li class="products">
+              <a href="./controller/product_deteals.php?id=<?php echo $value['id'] ?>">
+                <ul>
+                  <li class="product_img"><img src="./images/products/product_<?php echo $value['id'] ?>_1.jpg" width="180px" height="180px" alt=""></li>
+                  <li class="product_name"><?php echo $value['product_name']; ?></li>
+                  <li class="product_price">￥<?php echo $value['price']; ?></li>
+                  <li class="favorite"><img src="./images/image_materials/hart.jpg" width="30" height="30" alt=""></li>
+                </ul>
+              </a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php else : ?>
+        <p><?php echo $error_msg; ?></p>
+      <?php endif; ?>
     </div><!-- contents_area -->
     <footer>
       <div class="guide">
