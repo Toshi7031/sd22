fba759740e484d83aaccf9b01c375c186d9009a3
kozo804 <fazsegtwin@gmail.com>
2020-01-19 18:31:34 +0900

    アコーディオンを追加

--- js/login.js
+++ js/login.js
@@ -1,28 +1,9 @@
 $(function() {
-  $("#login").on("click", function() {
-    let name = $('#login_form [name="name"]').val();
-    let pass = $('#login_form [name="password"]').val();
-    //デバッグ用
-    // console.log(name);
-    // console.log(pass);
-
-    $.ajax({
-      url: "../model/ajax/read_user_data.php",
-      type: "POST",
-      catch: false,
-      dataType: "json",
-      data: { name, pass }
-    })
-      .done(function(data) {
-        //デバッグ用
-        console.log(data.name);
-        console.log(data.pass);
-        //正式な処理を追加する
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-        console.log(textStatus);
-        console.log(errorThrown);
-      });
+  const help = $('.help');
+  const help_area = $('.help_area');
+  // const toggle_class_name = 'hidden';
+  
+  help.on("click", function() {
+    help_area.slideToggle();
   });
 });
