139f56abf873c97bd307684dfe111d401ecc5be0
kozo804 <fazsegtwin@gmail.com>
2020-01-24 16:15:43 +0900

    セッション削除していたので保持に変更

--- controller/exhibition_check.php
+++ controller/exhibition_check.php
@@ -84,15 +84,6 @@ if(isset($_POST['exhibition']) && $_POST['exhibition'] == $_SESSION['check']){
     // require_once '../tpl/error.php';
   }
 
-  // セッション削除
-  if (ini_get('session.use_cookies')) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-    $params['path'], $params['domain'], $params['secure'], $params['httponly']
-    );
-  }
-  session_destroy();
-
   // リダイレクト
   $url = './exhibition_confirm.php';
   redirect($url);
