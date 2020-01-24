06afd879473bdd02fcc47d0b906afe78f495cc78
Toshi7031 <7031.toshi@gmail.com>
2020-01-24 16:41:26 +0900

    header部分を削除

---
+++ view/product_deteals.php
@@ -0,0 +1,96 @@
+    <div id="main_area">
+      <p class="product_image"><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="400px" height="400px"
+          alt=""></p>
+      <h2>商品名</h2>
+      <div class="reaction">
+        <ul>
+          <li>☆お気に入り</li>
+          <li>0</li>
+        </ul>
+      </div><!-- reaction -->
+      <div class="product_description">
+        <h3>商品の説明</h3>
+        <p>説明文</p>
+      </div>
+      <div class="product_information">
+        <h3>商品の情報</h3>
+        <table>
+          <tr>
+            <th>カテゴリー</th>
+            <td>家電&nbsp;&gt;&nbsp;PC・タブレット</td>
+          </tr>
+          <tr>
+            <th>商品の詳細</th>
+            <td>新品、未使用</td>
+          </tr>
+          <tr>
+            <th>配送料の負担</th>
+            <td>送料込（出品者負担）</td>
+          </tr>
+          <tr>
+            <th>配送の方法</th>
+            <td>未定</td>
+          </tr>
+          <tr>
+            <th>発送元の地域</th>
+            <td>大阪府</td>
+          </tr>
+          <tr>
+            <th>発送までの日数</th>
+            <td>1~2日で発送</td>
+          </tr>
+        </table>
+      </div>
+      <div class="exhibitor">
+        <h3>出品者</h3>
+        <ul>
+          <li><img src="../images/icons/yjimage.jpeg" width="220" height="220" alt=""></li>
+          <li>出品者名</li>
+        </ul>
+      </div>
+      <div class="related_goods">
+        <h3>関連商品</h3>
+        <ul>
+          <li class="goods">
+            <ul>
+              <li><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="200px" height="200px" alt=""></li>
+              <li>￥1000</li>
+            </ul>
+          </li>
+          <li class="goods">
+            <ul>
+              <li><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="200px" height="200px" alt=""></li>
+              <li>￥1000</li>
+            </ul>
+          </li>
+          <li class="goods">
+            <ul>
+              <li><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="200px" height="200px" alt=""></li>
+              <li>￥1000</li>
+            </ul>
+          </li>
+          <li class="goods">
+            <ul>
+              <li><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="200px" height="200px" alt=""></li>
+              <li>￥1000</li>
+            </ul>
+          </li>
+          <li class="goods">
+            <ul>
+              <li><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="200px" height="200px" alt=""></li>
+              <li>￥1000</li>
+            </ul>
+          </li>
+          <li class="goods">
+            <ul>
+              <li><img src="../images/products/61Yyhwxq32L._AC_SL1200_.jpg" width="100px" height="100px" alt=""></li>
+              <li>￥1000</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="fixed_area">
+        <p>1300円</p>
+        <p><a href="./buy_procedures.html">購入する</a></p>
+      </div>
+    </div>
