e02e8eed2a2682967f2a2d5f7b8cf6005c58c38f
kozo804 <fazsegtwin@gmail.com>
2020-01-24 14:14:51 +0900

    jsのpathを修正

--- controller/exhibition_check.php
+++ controller/exhibition_check.php
@@ -7,7 +7,7 @@ require_once '../model/login_check.php';
 
 $title = 'まさる堂/出品ページ';
 $css_file_name = 'exhibition_check.css';
-$js_file_name = '';
+$js_file_name = 'a';
 
 
 //登録を押したとき
@@ -50,9 +50,13 @@ if(isset($_POST['exhibition']) && $_POST['exhibition'] == $_SESSION['check']){
   $product_id = $product_id[0];
   ++$product_id;
 
+  echo '<pre>';
+  var_dump($post_info, $_SESSION['tmp_name']);
+  echo '</pre>';
+
   // 画像を本フォルダに移動
   for($i = 0; $i < $post_info['images_count']; ++$i) {
-    $file_info = getimagesize('../images/products/product_3_2');
+    $file_info = getimagesize('../images/tmp/' . $_SESSION['tmp_name'][$i]);
     if($file_info['mine'] == 'image/jpeg') {
       $extention = 'jpg';
     }
@@ -97,11 +101,11 @@ if(isset($_POST['exhibition']) && $_POST['exhibition'] == $_SESSION['check']){
 //postされたidをDBに照合
 $fetch_datas = array(
   'large_product_categories' => array(
-    'id' => (int)$_SESSION['large_product_categories'],
+    'id' => (int)$_SESSION['large_product_category_id'],
     'sql' => "SELECT large_product_categories.large_product_category FROM masarudoh.large_product_categories WHERE id = ?",
   ),
   'product_category' => array(
-    'id' => (int)$_SESSION['product_category_id'],
+    'id' => (int)$_SESSION['small_product_category_id'],
     'sql' => "SELECT small_product_categories.small_product_category FROM masarudoh.small_product_categories WHERE id = ?",
   ),
   'product_condition' => array(
@@ -148,7 +152,7 @@ if($_SESSION['postage_id']) {
 else {
   $postage = '着払い（購入者負担）';
 }
-$profit = intval($_SESSION['price'] * 0.9);   //販売利益計算
+$profit = ceil($_SESSION['price'] * 0.9);   //販売利益計算
 $check = random_string(8);  //DB書き込み処理に入るための割符
 $_SESSION['check'] = $check;  //割符を$_SESSIONに保存
 
