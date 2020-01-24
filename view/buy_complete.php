db69693f9e3ed4908d4fec688e5a498e77eede6d
Toshi7031 <7031.toshi@gmail.com>
2020-01-24 18:35:23 +0900

    buy_complete 作成

---
+++ view/buy_complete.php
@@ -0,0 +1,5 @@
+  <section class="area">
+    <p>購入が完了しました</p>
+    <p><img src="../images/image_materials/flower_white.png" width="200"></p>
+    <button><a href="../index.php">トップに戻る</a></button>
+  </section>
