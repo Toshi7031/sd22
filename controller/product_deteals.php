194671ce9d89daf86dc685d1409001ea9317ed22
Toshi7031 <7031.toshi@gmail.com>
2020-01-24 19:21:06 +0900

    DB取り出し

--- controller/product_deteals.php
+++ controller/product_deteals.php
@@ -1,6 +1,6 @@
 <?php
+  session_start();
   require '../config/config.php';
-
   /*---------------------------------------------
   変数宣言
   ---------------------------------------------*/
@@ -10,13 +10,34 @@
   /*---------------------------------------------
   値受取
   ---------------------------------------------*/
+  if(isset($_SESSION['login_id']) || !empty($_SESSION['login_id'])){
+    $login_id = $_SESSION['login_id'];
+  }
   if(isset($_GET['id'])){
     $product_id = (int)$_GET['id'];
   }
   else{
    header( "Location: ./product_view.php" );
   }
-   /
+
+  /*---------------------------------------------
+  DB取り出し
+  ---------------------------------------------*/
+
+  //sql作成
+  $sql = "SELECT product_name,price,description,small_product_category_id,large_product_category_id,product_condition_id,postage_id,send_method,state_id,days_to_send,progress,images_count,white_list FROM products WHERE id = {$product_id}";
+
+  //接続・取り出し
+  $mysqli = new mysqli(HOST,DB_USER,DB_PASS,DB_NAME);
+  $result = $mysqli -> query($sql);
+  $mysqli -> close();
+
+  //配列変換
+  $array_product = $result -> fetch_all(MYSQLI_ASSOC);
+  var_dump($array_product);
+  if($array_product[0]['white_list'] == $login_id){
+
+  }
 
   /*---------------------------------------------
   req html
