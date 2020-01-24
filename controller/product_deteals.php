98672499c2daad27c8b27bb3cd2d38a012368718
Toshi7031 <7031.toshi@gmail.com>
2020-01-24 16:44:53 +0900

    controller product_deteals.php 作成

---
+++ controller/product_deteals.php
@@ -0,0 +1,10 @@
+<?php
+  require '../config/config.php';
+
+  /*---------------------------------------------
+  req html
+  ---------------------------------------------*/
+  require '../view/header.php';
+  require '../view/product_deteals.php';
+  require '../view/footer.php';
+?>
