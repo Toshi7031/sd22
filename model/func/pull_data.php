2a6c0fba41aacc8d36dab773ebd1f3153be909d4
Toshi7031 <7031.toshi@gmail.com>
2020-01-25 17:50:08 +0900

    データベースphp ソース化プログラム

---
+++ model/func/pull_data.php
@@ -0,0 +1,30 @@
+<?php
+/*--------------------------
+データベースを配列変換
+---------------------------*/
+  require '../../config/config.php';
+  $mysqli = new mysqli(HOST,DB_USER,DB_PASS,DB_NAME);
+  $sql = "select state from states";
+  $result = $mysqli -> query($sql);
+  $array = $result -> fetch_all(MYSQLI_NUM);
+  $mysqli -> close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  $array = [ <br>
+<?php
+  foreach($array as $data):
+?>
+'<?php echo $data[0]?>',<br>
+
+<?php endforeach; ?>
+]
+</body>
+</html>
