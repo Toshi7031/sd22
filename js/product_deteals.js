83986f0f5fa6b176d743c4a99683dad368006e4b
kozo804 <fazsegtwin@gmail.com>
2020-01-26 11:56:55 +0900

    slickを追加

--- js/product_deteals.js
+++ js/product_deteals.js
@@ -2,6 +2,8 @@ $(function() {
   const product_image = $('.product_image');
   var image_count = product_image.find('img').length;
 
+
+  // スリックのオプションをつける
   if(image_count >= 2) {
     product_image.slick();
   }
